0d69d38ade1300fb8d8f32cc4d5dc488278c963f
prakash9320 <prakashshukla275@gmail.com>
2022-01-22 14:29:55 +0530

    add validation and user Exits

--- router/auth.js
+++ router/auth.js
@@ -1,14 +1,32 @@
 const express = require('express');
 const router = express.Router();
 
+require('../db/conn');
+const user = require("../model/userSchema");
+
 router.get('/', (req, res) => {
     res.send(`Hello world from the server rotuer js`);
 });
 
 router.post('/register', (req, res) => {
-    console.log(req.body);
-    res.json({ details: req.body });
-    // res.send("mera register page");
+         // call all objects in one line 
+    const {name ,email,  phone, work,  password, cpassword } = req.body;
+
+    if(!name ||!email|| !phone || !work|| !password || !cpassword){
+        return res.status(422).json({error :"Pleases Fill The Field Properly"});
+    }
+     user.findOne({email:email}).then((userExist)=>{
+          if(userExist){
+            return res.status(422).json({error :"Email Alredy Exits"}); 
+          }
+
+          const User = new user({name ,email,  phone, work,  password, cpassword });
+          User.save().then(()=>{
+             res.status(201).json({messege:"user Register successfully "})      
+        }).catch((err) =>{res.status(500).json({error:"Fail To Registered"})})
+
+     }).catch((err) =>{console.log(err); })
+     
 });
 
 module.exports = router;
