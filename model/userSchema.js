ce98eded2c5950a11e5681db81b024185bc4501c
prakash9320 <prakashshukla275@gmail.com>
2022-01-28 15:52:13 +0530

    import jwt and add some function

--- model/userSchema.js
+++ model/userSchema.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken')
 const mongooose = require('mongoose');
  const bcrypt = require('bcryptjs');
 const userSchema = new mongooose.Schema({
@@ -24,7 +25,15 @@ const userSchema = new mongooose.Schema({
     cpassword: {
          type: String,
         required:true
-    }
+    },
+    tokens:[
+        {
+            token:{
+                type: String,
+                required:true 
+            }
+        }
+    ]
 })
 
 
@@ -38,6 +47,18 @@ userSchema.pre('save', async function(next){
    }
    next();
 });
+
+ userSchema.methods.generateAuthToken = async function (){
+     try{
+        let token = jwt.sign({_id:this._id}, process.env.SECRET_KEY); 
+        this.tokens = this.tokens.concat({token:token});
+        await this.save();
+        return token;
+     } catch(err){
+       console.log(err)
+     }
+ }
+
   // collection creation 
 const User = mongooose.model('USER', userSchema);
 
